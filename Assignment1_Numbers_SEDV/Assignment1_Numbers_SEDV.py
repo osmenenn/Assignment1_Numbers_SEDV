0ae92fc709fd6d9c43388433a247ed425fb05988
Owner <Owner@DESKTOP-5MQ734E>
2022-09-06 20:25:02 -0400

    Add project files.

---
+++ Assignment1_Numbers_SEDV/Assignment1_Numbers_SEDV.py
@@ -0,0 +1,19 @@
+#Assingment 1, NUmbers
+#Last Updated: 9/5/2022
+#Author Ismoil Boboev
+#SEVD 220
+
+
+
+
+
+
+print("Hello World")
+years_list = [1999, 2000, 2001, 2002, 2003, 2004, 2005]
+
+
+things_list = ["Mozzarela", "Cinderella", "Salmonella"]
+print(things_list)
+
+THINGS_list = Mozzerela, 'Cindrella', 'Salmonela'
+print(THINGS_list)
